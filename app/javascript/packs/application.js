033840c472472778cec8c930be1e8d27c0c1fb27
Romain Hethener <romain.hethener@gmail.com>
2020-07-22 19:33:59 +0200

    right click disabled

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,20 @@ require("jquery")
 
 import "bootstrap";
 import { MediaPlayer } from '../components/video';
+import $ from 'jquery';
 // import { Slider } from '../components/slider';
 
-$( document ).ready(function() { $("#query").focus(); });
+$( document ).ready(function() { $("#query").focus();
 
 MediaPlayer();
 // Slider();
+// $('slideshow-container').each(function() {
+
+// $(this)[0].oncontextmenu = function() {
+
+// alert("We're sorry. Right-Click is not available");
+
+// return false;
+// };
+//   });
+});
